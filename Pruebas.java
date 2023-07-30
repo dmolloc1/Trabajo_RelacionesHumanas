d51daf262ade9b8a05cec623837bad7458a3c75c
JhonatanDczel <jariasq@unsa.edu.pe>
2023-07-29 20:54:41 -0500

    Especificaciones para las pruebas de la calculadora

--- Pruebas.java
+++ Pruebas.java
@@ -1,6 +1,10 @@
 // Aqui se haran las pruebas de la calculadora
 class Pruebas{
 	public static void main(String[] args){
-		
+      //Las pruebas para la calculadora
+      //consistiran en dos enteros
+      //que representen las notas de dos unidades
+      //la salida debera ser un unico numero
+      //entero que muestre el promedio ponderado del curso
 	}
 }
