00cad046c3b0cb124f3ce818f410166d9a878479
JosueClaudioQP <jquispepauc@unsa.edu.pe>
2023-07-29 20:15:38 -0500

    Calculadora normal solo de suma, resta, multiplicacion y division

---
+++ Calculadora.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double resultado;
+
+        System.out.println("Ingrese el primer número:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Ingrese el segundo número:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Elija una operación:");
+        System.out.println("1 - Suma");
+        System.out.println("2 - Resta");
+        System.out.println("3 - Multiplicación");
+        System.out.println("4 - División");
+        int opcion = scanner.nextInt();
+
+        switch (opcion) {
+            case 1:
+                resultado = num1 + num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case 2:
+                resultado = num1 - num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case 3:
+                resultado = num1 * num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case 4:
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                    System.out.println("Resultado: " + resultado);
+                } else {
+                    System.out.println("Error: No se puede dividir entre cero.");
+                }
+                break;
+            default:
+                System.out.println("Opción no válida. Por favor, elija una opción válida.");
+        }
+
+        scanner.close();
+    }
+}
